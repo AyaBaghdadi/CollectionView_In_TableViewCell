dff2643b08b56a1b6c4f24b6a5265cf8b8c41fc3
Aya <ayabaghdadi83@yahoo.com>
2021-08-19 21:46:41 +0200

    Initial Commit

---
+++ Technicalisto_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technicalisto_Project
+//
+//  Created by OSX on 19/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
